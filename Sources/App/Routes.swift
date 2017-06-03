feec2b3e4b4589c7b8b2d7421b7a0a806427b1ff
Erik <nuclear.ace@gmail.com>
2017-06-03 18:50:42 -0400

    refactor route building

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -13,16 +13,19 @@ final class Routes: RouteCollection {
     }
 
     func build(_ builder: RouteBuilder) throws {
-        builder.get {req in
-            guard req.auth.isAuthenticated(User.self) else { return Response(redirect: "/users/login") }
+        try addViewRoutes(builder)
+        try addApiRoutes(builder)
 
-            return Response(redirect: "/wall")
+        builder.socket("ws") {req, ws in
+            Gateway.shared.add(ws)
+
+            ws.onClose = {ws, code, reason, clean in
+                Gateway.shared.remove(ws)
+            }
         }
+    }
 
-        builder.grouped(passwordProtect).get("wall") {req in
-            return try self.view.make("post.html")
-        }
-
+    private func addApiRoutes(_ builder: RouteBuilder) throws {
         builder.grouped("api").grouped(passwordProtect).group("post") {postGroup in
             postGroup.post(handler: PostController.create)
         }
@@ -32,6 +35,18 @@ final class Routes: RouteCollection {
             userGroup.post("new", handler: UserController.create)
             userGroup.grouped(passwordProtect).post("login", handler: UserController.login)
             userGroup.post("logout", handler: UserController.logout)
+        }
+    }
+
+    private func addViewRoutes(_ builder: RouteBuilder) throws {
+        builder.get {req in
+            guard req.auth.isAuthenticated(User.self) else { return Response(redirect: "/users/login") }
+
+            return Response(redirect: "/wall")
+        }
+
+        builder.grouped(passwordProtect).get("wall") {req in
+            return try self.view.make("post.html")
         }
 
         builder.group("users") {userGroup in
@@ -57,14 +72,6 @@ final class Routes: RouteCollection {
 
             userGroup.grouped(passwordProtect).get("logout") {req in
                 return try self.view.make("users/logout.html")
-            }
-        }
-
-        builder.socket("ws") {req, ws in
-            Gateway.shared.add(ws)
-
-            ws.onClose = {ws, code, reason, clean in
-                Gateway.shared.remove(ws)
             }
         }
     }
